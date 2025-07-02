9fb8ea74f1358b0c4da83b43b86f94c13925d0e7
Jose <jm.bebor@gmail.com>
2025-07-02 11:46:04 +0000

    [WIP]delete

--- src/components/ContactForm.jsx
+++ src/components/ContactForm.jsx
@@ -4,8 +4,34 @@ export const ContactForm = () => {
 
     const [name, setName] = useState("")
     const [email, setEmail] = useState("")
-    const [phone, sePhone] = useState("")
+    const [phone, setPhone] = useState("")
     const [address, setAddress] = useState("")
+
+    function addContact({ name }, { email }, { phone }, { address }) {
+
+        fetch('https://playground.4geeks.com/contact/agendas/jmb/contacts', {
+            method: "POST",
+            body: JSON.stringify({
+                "name": name,
+                "phone": email,
+                "email": phone,
+                "address": address
+            }),
+            headers: {
+                "Content-Type": "application/json"
+            }
+        })
+            .then((response) => {
+                
+                if (response.status === 201) {
+                    console.log(response);
+                }
+
+                return response.json()
+            })
+            .then((data) => console.log(data))
+            .catch((error) => console.log(error))
+    }
 
     return (
 
@@ -13,34 +39,34 @@ export const ContactForm = () => {
         <div className="container w-75">
             <form>
 
-            <div className="mb-3 text-start justify-content-start">
+                <div className="mb-3 text-start justify-content-start">
 
-                <label htmlFor="exampleFormControlInput1" className="form-label">Full Name</label>
-                <input type="text" className="form-control" id="exampleFormControlInput1" placeholder="Full Name"/>
-                
-            </div>
-            <div className="mb-3 text-start justify-content-start">
+                    <label htmlFor="exampleFormControlInput1" className="form-label">Full Name</label>
+                    <input type="text" className="form-control" id="exampleFormControlInput1" placeholder="Full Name" value={name} onChange={(e) => setName(e.target.value)} />
 
-                <label htmlFor="exampleFormControlInput2" className="form-label">Email</label>
-                <input type="email" className="form-control" id="exampleFormControlInput2" placeholder="Enter Email"/>
-                
-            </div>
-            <div className="mb-3 text-start justify-content-start">
+                </div>
+                <div className="mb-3 text-start justify-content-start">
 
-                <label htmlFor="exampleFormControlInput3" className="form-label">Phone</label>
-                <input type="text" className="form-control" id="exampleFormControlInput3" placeholder="Enter Phone"/>
-                
-            </div>
-            <div className="mb-3 text-start justify-content-start">
+                    <label htmlFor="exampleFormControlInput2" className="form-label">Email</label>
+                    <input type="email" className="form-control" id="exampleFormControlInput2" placeholder="Enter Email" value={email} onChange={(e) => setEmail(e.target.value)} />
 
-                <label htmlFor="exampleFormControlInput4" className="form-label">Address</label>
-                <input type="text" className="form-control" id="exampleFormControlInput4" placeholder="Enter Address"/>
-                
-            </div>
-            
-            <button className="btn btn-primary w-100">Save</button>
+                </div>
+                <div className="mb-3 text-start justify-content-start">
+
+                    <label htmlFor="exampleFormControlInput3" className="form-label">Phone</label>
+                    <input type="text" className="form-control" id="exampleFormControlInput3" placeholder="Enter Phone" value={phone} onChange={(e) => setPhone(e.target.value)} />
+
+                </div>
+                <div className="mb-3 text-start justify-content-start">
+
+                    <label htmlFor="exampleFormControlInput4" className="form-label">Address</label>
+                    <input type="text" className="form-control" id="exampleFormControlInput4" placeholder="Enter Address" value={address} onChange={(e) => setAddress(e.target.value)} />
+
+                </div>
+
+                <button className="btn btn-primary w-100" onClick={() => addContact({name},{email},{phone},{address})}>Save</button>
             </form>
-            
+
         </div>
     )
 }
